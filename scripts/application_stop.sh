b1516edf84a6b5dff17699ae8948c6c28f577c99
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-17 22:12:52 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,16 +1,4 @@
-
 #!/bin/bash
-
-echo "Stopping any existing Node.js servers..."
-
-# Check if any Node.js process is running
-if pgrep node > /dev/null
-then
-    echo "Node.js server found. Process ID(s):"
-    pgrep node
-    echo "Stopping Node.js server..."
-    pkill node
-else
-    echo "No Node.js server running"
-fi
-
+#Stopping existing node servers
+echo "Stopping any existing node servers"
+pkill node
